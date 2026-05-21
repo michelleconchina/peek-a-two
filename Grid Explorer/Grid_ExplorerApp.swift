d393a75d417177fd399507b61660b5662d5d5f77
Michelle Conchina <conchina.michelle@gmail.com>
2026-05-21 16:14:17 +0800

    Initial Commit

---
+++ Grid Explorer/Grid_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_ExplorerApp.swift
+//  Grid Explorer
+//
+//  Created by Michelle Conchina on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Grid_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
